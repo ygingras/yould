21fc8239a26d3fbb706eea3cbfd9d268c882c80b
Yannick Gingras <ygingras@ygingras.net>
2021-12-12 16:32:54 -0700

    updated copyright

--- yould/dl-kjb.py
+++ yould/dl-kjb.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+# Copyright 2007, 2020 Yannick Gingras <ygingras@ygingras.net>
+
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
